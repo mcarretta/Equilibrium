0aa1b0b15370bffd20d876cb256a757fe6944cab
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-11-16 12:17:10 +0100

    added door opened by switches && FPS counter

---
+++ Equilibrium/Assets/Scripts/Environment/Locked Door/LockedDoor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockedDoor : MonoBehaviour
+{
+    [SerializeField] List<LightTrigger> triggerList = new List<LightTrigger>(); //lista di bottoni che attivano la porta
+
+    void Start()
+    {
+        StartCoroutine(UnlockTest(0.33f));
+    }
+
+    //coroutine che controlla ogni testTime secondi se la porta è aperta
+    //termina quando la porta è aperta
+    private IEnumerator UnlockTest(float testTime)
+    {   while (true)
+        {
+            bool unlocked = true;
+            foreach (LightTrigger trigger in triggerList)
+                unlocked = unlocked & trigger.IsTriggered();
+
+            if (unlocked)
+            {
+                print("door unlocked");
+                yield break;
+            }
+
+            unlocked = true;
+            yield return new WaitForSeconds(testTime);
+        }
+    }
+}
---
+++ Equilibrium/Assets/Scripts/ReloadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
