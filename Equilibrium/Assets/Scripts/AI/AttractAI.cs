ff9f6fd7d7ba32a50291d724b2906448b79d8e23
Diego Piccinotti <elimdor@live.it>
2019-11-18 13:15:39 +0100

    Implemented AI chase - no wandering

---
+++ Equilibrium/Assets/Scripts/AI/AttractAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttractAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool hasTriggeredSwitch;
+    public bool condition2;
+    public bool condition3;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
