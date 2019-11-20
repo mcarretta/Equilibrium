c8d920ca908f7b249617784146f862022f151049
Diego Piccinotti <elimdor@live.it>
2019-11-20 13:14:12 +0100

    Implemented enemy death by bullets and better chase

---
+++ Equilibrium/Assets/Scripts/AI/AICombat.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AICombat : MonoBehaviour
+{
+    private float _health;
+    private bool _canFade;
+    private Color _startColor;
+    private MeshRenderer _mMeshRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _health = 1f;
+        _canFade = false;
+        _mMeshRenderer = GetComponent<MeshRenderer>();
+        //_startColor = Color.red;
+        //_endColor = 
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            hit(25f);
+        }
+    }
+
+    
+    public void hit(float energy)
+    {
+        _health -= energy;
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+            
+    }
+}
--- Equilibrium/Assets/Scripts/Player/LightBullet.cs
+++ Equilibrium/Assets/Scripts/Player/LightBullet.cs
@@ -8,19 +8,21 @@ public class LightBullet : MonoBehaviour
 {
     [SerializeField] private float speed = 10f; //velocità del proiettile
     [SerializeField] private float lifetime = 5f; //si distrugge dopo n secondi da quando è stato sparato
-    private float timer; //timer per distruggere l'oggetto
+    private float _timer; //timer per distruggere l'oggetto
+    private float _energy;
 
     private void Start()
     {
-        timer = lifetime;    
+        _timer = lifetime;
+        _energy = 1f;
     }
 
     void Update()
     {
         transform.position = transform.position + transform.forward * speed * Time.deltaTime; //muovo il proiettile senza fisica
 
-        timer -= Time.deltaTime;
-        if (timer <= 0)
+        _timer -= Time.deltaTime;
+        if (_timer <= 0)
             Destroy(gameObject);
     }
 
@@ -28,5 +30,9 @@ public class LightBullet : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if (other.CompareTag("Enemy"))
+        {
+            other.GetComponent<AICombat>().hit(_energy);
+        }
     }
 }
