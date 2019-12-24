d128ceaebdb0998a68538f068a10372fe7d6ca51
CriptHunter <l.dalessandro5@campus.unimib.it>
2019-12-24 11:36:37 +0100

    added main menu && updated levels manager

--- Equilibrium/Assets/Scripts/Levels/LevelsManager.cs
+++ Equilibrium/Assets/Scripts/Levels/LevelsManager.cs
@@ -7,13 +7,42 @@ public class LevelsManager : Singleton<LevelsManager>
 {
     [SerializeField] private List<string> levels;
     [SerializeField] private string deathMenu;
+    [SerializeField] private string mainMenu;
+
     private int index = 0;
 
+    public List<string> getLevels()
+    {
+        return new List<string>(levels);
+    }
+
+    //carica un livello dato l'indice nella lista
+    public void LoadLevelByIndex(int i)
+    {
+        if (index + 1 >= levels.Count || index < 0)
+            print("indice inesistente");
+        else
+        {
+            index = i;
+            SceneManager.LoadScene(levels[index]);
+        }
+    }
+
+    //carica il livello attivo dopo la morte o con retry
     public void LoadCurrentLevel()
     {
         SceneManager.LoadScene(levels[index]);
     }
 
+    //carica il primo livello del gioco con new game
+    public void LoadFirstLevel()
+    {
+        print("ready to load the first level");
+        index = 0;
+        SceneManager.LoadScene(levels[index]);
+    }
+
+    //carica il livello successivo quando si arriva alla fine di un livello
     public void LoadNextLevel()
     {
         print("ready to load next level... index == " + index);
@@ -26,6 +55,7 @@ public class LevelsManager : Singleton<LevelsManager>
         SceneManager.LoadScene(levels[index]);
     }
 
+    //carica livello precedente
     public void LoadPreviousLevel()
     {
         if (index <= 0)
@@ -38,6 +68,12 @@ public class LevelsManager : Singleton<LevelsManager>
         SceneManager.LoadScene(levels[index]);
     }
 
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene(mainMenu);
+    }
+
+    //menu quando si muore
     public void LoadDeathMenu()
     {
         SceneManager.LoadScene(deathMenu);
---
+++ Equilibrium/Assets/Scripts/Menu/ShowHidePanel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHidePanel : MonoBehaviour
+{
+    public GameObject panel;
+    bool state;
+
+    public void ShowHide()
+    {
+        state = !state;
+        panel.gameObject.SetActive(state);
+    }
+}
