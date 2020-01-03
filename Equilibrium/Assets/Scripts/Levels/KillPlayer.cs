1fbae0802dced3fca5660f194fb406ced522a152
Diego Piccinotti <elimdor@live.it>
2020-01-03 19:28:55 +0100

    Death animation + black fade in

--- Equilibrium/Assets/Scripts/Levels/KillPlayer.cs
+++ Equilibrium/Assets/Scripts/Levels/KillPlayer.cs
@@ -5,9 +5,26 @@ using UnityEngine.SceneManagement;
 
 public class KillPlayer : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public GameObject ui;
+    private static readonly int FadeOut = Animator.StringToHash("FadeOut");
+    private IEnumerator OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.layer == LayerMask.NameToLayer("Player")) //se è un oggetto sul layer player
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player"))
+
+        {
+            //se è un oggetto sul layer player
+            Animator animator = ui.GetComponent<Animator>();
+            animator.SetTrigger(FadeOut);
+            Destroy(gameObject);
+            yield return new WaitForSeconds(1);
             LevelsManager.Instance.LoadDeathMenu();
+            
+        }
+    }
+
+    public void LoadDeathMenu()
+    {
+        
+
     }
 }
