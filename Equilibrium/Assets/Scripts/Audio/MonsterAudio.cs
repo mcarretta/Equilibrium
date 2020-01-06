726682f0620aafc3342f35823572b2b9fa3643dc
Matteo Carretta <carretta.mtt@gmail.com>
2020-01-06 12:47:49 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Equilibrium/Assets/Scripts/Audio/MonsterAudio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAudio : MonoBehaviour
+{
+    public AudioClip ScreamSound;
+
+    public AudioSource Monster;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
