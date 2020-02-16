0df3c54e0181325cf5ff939baee69f5778aa1bd9
CriptHunter <lorenzo.dalessandro1@studenti.unimi.it>
2020-02-16 16:26:23 +0100

    polimi splash screen and team names

---
+++ Equilibrium/Assets/Scripts/Menu/OpenWithFade.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OpenWithFade : MonoBehaviour
+{
+    [SerializeField] Image img;
+
+    void Start()
+    {
+        StartCoroutine(Fade(3f));
+    }
+
+    IEnumerator Fade(float time)
+    {
+        yield return new WaitForSeconds(time);
+        img.CrossFadeAlpha(0, 1.2f, false);
+        yield return new WaitForSeconds(time + 1.2f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
